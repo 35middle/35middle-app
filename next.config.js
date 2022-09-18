aeb35c1af94ea27ba36878487945663e42a76d85
leo <shixun.liu1023@gmail.com>
2022-09-19 00:08:17 +1000

    build: deploy app to lambda

--- next.config.js
+++ next.config.js
@@ -4,14 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
-  eslint: {
-    dirs: ['.'],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
+  target: 'serverless',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
 });
