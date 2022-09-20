7b9582ae75ab124e26f7fdcd9c8bb2ee2f6c0caf
Leo Liu <leo.liu@domain.com.au>
2022-09-20 21:40:11 +1000

    feat: clear default pages created by boilerplate

--- src/pages/__tests__/about.test.tsx
+++ src/pages/__tests__/about.test.tsx
@@ -1,14 +1,14 @@
 import { render } from '@testing-library/react';
 
-import Index from '@/pages/index';
+import About from '@/pages/about';
 
 // The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
 // The mock has been moved to `__mocks__` folder to avoid duplication
 
-describe('Index page', () => {
+describe('About page', () => {
   describe('Render method', () => {
-    it('should have h1 tag', () => {
-      render(<Index />);
+    it('should have two paragraphs of `Lorem ipsum`', () => {
+      render(<About />);
       expect(1).toBe(1);
     });
   });
---
+++ src/pages/__tests__/index.test.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+
+import Index from '@/pages';
+
+// The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
+// The mock has been moved to `__mocks__` folder to avoid duplication
+
+describe('Index page', () => {
+  describe('Render method', () => {
+    it('should have h1 tag', () => {
+      render(<Index />);
+      expect(1).toBe(1);
+    });
+  });
+});
