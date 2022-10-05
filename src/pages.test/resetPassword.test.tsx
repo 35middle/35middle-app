0d7c3545a91cc50c3e6a6c76b69bc4f19414a6c7
Lucasshao <yilongshao2020@gmail.com>
2022-10-06 09:39:27 +1100

    feat: reset password page

---
+++ src/pages.test/resetPassword.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+
+import ResetPassword from '../pages/resetPassword';
+
+it('should have button tag', () => {
+  render(<ResetPassword />);
+  const submitButton = screen.getByRole('button');
+  expect(submitButton).toBeVisible();
+});
+
+it('render password input', () => {
+  render(<ResetPassword />);
+  const inputPs = screen.getByTestId('password');
+  expect(inputPs).toBeInTheDocument();
+  // expect(inputPs).toHaveAttribute('password');
+});
