69290347b496180d7fda96b29bb6c1bfef8b5992
thomasni91 <89690735+thomasni91@users.noreply.github.com>
2022-10-05 20:48:01 +1100

    Update register.tsx

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -65,7 +65,7 @@ const Register = () => {
             width="240"
             height="240"
           />
-          <Typography variant="h1">Welcome to Register9sdfsdfasdfsdfsddf</Typography>
+          <Typography variant="h1">Welcome to Register</Typography>
         </Box>
         <form
           className="flex flex-col items-center justify-center"
