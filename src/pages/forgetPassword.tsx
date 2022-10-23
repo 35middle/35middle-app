69d158c9ef58f0bb0c0e14c263dcfeadd5e56ae1
Lydia <mengyao1227@gmail.com>
2022-10-23 20:25:44 +1100

    feat: refactor forgetpassword page with formik

--- src/pages/forgetPassword.tsx
+++ src/pages/forgetPassword.tsx
@@ -1,26 +1,126 @@
-import Image from 'next/image';
+import {
+  Alert,
+  Box,
+  Button,
+  Snackbar,
+  TextField,
+  Typography,
+} from '@mui/material';
+import type { FormikProps } from 'formik';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import * as React from 'react';
+import * as yup from 'yup';
 
-import { Meta } from '@/layouts/Meta';
-import { Main } from '@/templates/Main';
+const ForgetPasswordSchema = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Required'),
+});
 
-const ForgetPassword = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
-    <div className="fixed inset-1/4 flex h-3/6 w-3/6 flex-col items-center justify-center">
-      <Image
-        src="/assets/images/logo.png"
-        alt="logo"
-        width={200}
-        height={200}
-      />
-      <p className="mt-10">Magic Link to reset password</p>
-      <input
-        type="text"
-        placeholder="email"
-        className="input-bordered input-success input mt-5 w-full max-w-xs"
-      />
-      <button className="btn-outline btn-accent btn mt-3">Submit</button>
-    </div>
-  </Main>
-);
+interface FormValues {
+  email: string;
+}
+
+const ForgetPassword = () => {
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const onSubmit = async (values: any, actions: any) => {
+    try {
+      const response = await fetch('/api/forgetPassword', {
+        method: 'POST',
+        body: JSON.stringify(values),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        // jump to login page
+      } else {
+        setErrorMsg(data.message);
+      }
+    } catch (e: any) {
+      setErrorMsg(e.message);
+    } finally {
+      actions.resetForm();
+    }
+  };
+
+  const handleAlertClose = () => {
+    setErrorMsg('');
+  };
+
+  const {
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  }: FormikProps<FormValues> = useFormik<FormValues>({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: ForgetPasswordSchema,
+    onSubmit,
+  });
+
+  return (
+    <>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={!!errorMsg}
+        autoHideDuration={6000}
+        onClose={handleAlertClose}
+      >
+        <Alert
+          onClose={handleAlertClose}
+          severity="error"
+          sx={{ width: '100%' }}
+        >
+          {errorMsg}
+        </Alert>
+      </Snackbar>
+      <Box className="flex h-screen items-center justify-center bg-background">
+        <Box className="w-full max-w-md space-y-8">
+          <Box className="flex flex-col items-center justify-center">
+            <img
+              src="/assets/images/35middle.png"
+              alt="logo"
+              width="240"
+              height="240"
+            />
+            <Typography variant="h2">
+              Get magic link to reset password
+            </Typography>
+          </Box>
+          <form
+            className="flex flex-col items-center justify-center"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              id="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="Email"
+              type="email"
+              className="mb-4 w-full"
+              error={touched.email && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+            />
+            <Box className="mt-4 flex w-full items-center justify-between">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="large"
+              >
+                Submit
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 export default ForgetPassword;
