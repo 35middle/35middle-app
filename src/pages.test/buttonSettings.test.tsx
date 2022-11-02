d2af6542ec08bf060b40a56466c7b613253182f6
Xinra <scorpioliu1108@gmail.com>
2022-11-02 17:50:10 +0800

    feat: create button setting page without button position

--- src/pages.test/buttonSettings.test.tsx
+++ src/pages.test/buttonSettings.test.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { render, screen } from '@testing-library/react';
 
 import ButtonSettings from '../components/buttonSettings';
@@ -9,3 +10,17 @@ describe('ButtonSettings page', () => {
     expect(heading).toBeVisible();
   });
 });
+||||||| parent of fb7e8c5 (feat: create button setting page without button position)
+=======
+import { render, screen } from '@testing-library/react';
+
+import ButtonSettings from '../pages/buttonSettings';
+
+describe('ButtonSettings page', () => {
+  it('should render heading, textfield', () => {
+    render(<ButtonSettings />);
+    const heading = screen.getByRole('heading', { name: 'Button Settings' });
+    expect(heading).toBeVisible();
+  });
+});
+>>>>>>> fb7e8c5 (feat: create button setting page without button position)
