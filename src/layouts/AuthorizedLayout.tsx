23c2d74fb210bac26aa6f0c000c38ce76d0730eb
Zhou Fang <zhouf66@gmail.com>
2022-11-03 18:38:46 +1100

    feat: rtk setup

--- src/layouts/AuthorizedLayout.tsx
+++ src/layouts/AuthorizedLayout.tsx
@@ -1,20 +1,46 @@
 import type { ReactNode } from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Footer from '@/components/Footer';
+import NavBar from '@/components/NavBar';
+import { accountActions } from '@/store/index';
 import { AppConfig } from '@/utils/AppConfig';
-
-import NavBar from '../components/NavBar';
 
 type Props = {
   children: ReactNode;
 };
 
-const AuthorizedLayout = ({ children }: Props) => (
-  <div className="flex h-screen w-full flex-col text-gray-700 antialiased">
-    <NavBar title={AppConfig.title} />
-    <main className="flex-1">{children}</main>
-    <Footer />
-  </div>
-);
+const AuthorizedLayout = ({ children }: Props) => {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const getAccountId = async () => {
+      try {
+        // const response = await fetch('', {
+        //   method: 'GET',
+        // });
+        // const data = await response.json();
+        // if (response.ok) {
+        //   dispatch(accountActions.setAccountId(data.accountId));
+        // } else {
+        //   dispatch(accountActions.setAccountId(data.accountId));
+        // }
+        const data = { accountId: '123' };
+        dispatch(accountActions.setAccountId(data.accountId));
+      } catch (e: any) {
+        console.log(e);
+      }
+    };
+    getAccountId();
+  }, []);
+
+  return (
+    <div className="flex h-screen w-full flex-col text-gray-700 antialiased">
+      <NavBar title={AppConfig.title} />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+};
 export default AuthorizedLayout;
