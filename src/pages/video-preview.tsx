b03f35a9e937e0d3aa412d216abe87fdbeb7a0d3
Lucasshao <yilongshao2020@gmail.com>
2022-11-05 16:24:52 +1100

    feat: create button component

---
+++ src/pages/video-preview.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import CustomButton from '../components/CustomButton';
+
+const videoPreview = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+
+  return (
+    <div className="content">
+      <div className="content-video">
+        <video width="600" height="400" controls autoPlay muted>
+          <source src={`${router.basePath}/demo.mp4`} type="video/mp4" />
+        </video>
+        <CustomButton name={'ADD TO CART'} />
+      </div>
+    </div>
+  );
+};
+
+export default videoPreview;
