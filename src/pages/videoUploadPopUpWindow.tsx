e061a6e2764aed933a335148f026816380ec9154
Anita <scorpioliu1108@gmail.com>
2022-11-07 07:55:27 +0800

    feat: add textfield and button to video upload pop  up

--- src/pages/videoUploadPopUpWindow.tsx
+++ src/pages/videoUploadPopUpWindow.tsx
@@ -1,9 +1,20 @@
-import { Box } from '@mui/material';
+import { Box, Button, Card, TextField, Typography } from '@mui/material';
 
 const VideoUploadPopUpWindow = () => {
   return (
     <>
-      <Box className="bg-white p-10">video upload</Box>
+      <Card>
+        <Box className="bg-white p-10">
+          <Typography variant="h5">video upload</Typography>
+        </Box>
+        <div className="flex flex-col p-10">
+          <div>
+            <TextField label="Viedeo title" variant="outlined" />
+            <TextField label="Viedeo description" variant="outlined" />
+          </div>
+          <Button>Upload Video</Button>
+        </div>
+      </Card>
     </>
   );
 };
