ce020972b93acc4b24aefec9052206ec995d45af
Anita <scorpioliu1108@gmail.com>
2022-11-06 11:34:24 +0800

    feat: create loader spinner and basic page look

---
+++ src/components/LinearWithValueLabel.tsx
@@ -0,0 +1,45 @@
+import Box from '@mui/material/Box';
+import type { LinearProgressProps } from '@mui/material/LinearProgress';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+const LinearProgressWithLabel = (
+  props: LinearProgressProps & { value: number }
+) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+const LinearWithValueLabel = () => {
+  const [progress, setProgress] = React.useState(10);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 10 : prevProgress + 10
+      );
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <LinearProgressWithLabel value={progress} />
+    </Box>
+  );
+};
+
+export default LinearWithValueLabel;
