ae28263f69779795bb6e5f5c3044e44f4db610fa
Lucasshao <yilongshao2020@gmail.com>
2022-11-11 15:06:48 +1100

    feat: create buttonSettings props

--- src/components/buttonSettings.tsx
+++ src/components/buttonSettings.tsx
@@ -9,26 +9,58 @@ import {
 } from '@mui/material';
 import * as React from 'react';
 
-import ButtonPosition from '@/pages/button/buttonPosition';
+// import ButtonPosition from '@/pages/button/buttonPosition';
 
-const ButtonSettings = () => {
-  const [click, setClick] = React.useState(false);
-  // console.log(click);
-  const handleClickButtonStyle = () => {
-    setClick(!click);
+const ButtonSettings = (props: any) => {
+  // const [click, setClick] = React.useState(false);
+  // // console.log(click);
+  // const handleClickButtonStyle = () => {
+  //   setClick(!click);
+  // };
+  const { buttonStyle, setButtonStyle } = props;
+  const { name, text, top, left, url } = buttonStyle;
+
+  const NamehandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setButtonStyle((v: any) => ({ ...v, name: value }));
+  };
+  const TexthandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setButtonStyle((v: any) => ({ ...v, text: value }));
+  };
+  const TophandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setButtonStyle((v: any) => ({ ...v, top: value }));
+  };
+  const LefthandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setButtonStyle((v: any) => ({ ...v, left: value }));
   };
 
-  const buttonStyle = {
-    Clickable: {
-      border: '2px solid #000',
-    },
+  const UrlhandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setButtonStyle((v: any) => ({ ...v, url: value }));
+  };
+
+  const handleClickButtonStyle = (value: string) => {
+    setButtonStyle((v: any) => ({ ...v, style: value }));
+  };
+
+  const handleClickButtonSize = (value: string) => {
+    setButtonStyle((v: any) => ({ ...v, size: value }));
+  };
+
+  const border = {
+    // Clickable: {
+    border: '3px solid #000',
+    // },
   };
 
   return (
     <>
       <Card
         className="m-5 flex justify-center px-10 py-5"
-        sx={{ width: 400, height: 700, flexDirection: 'column' }}
+        sx={{ width: 400, height: 800, flexDirection: 'column' }}
       >
         <Typography variant="h3" align="center" className="mb-2 flex">
           Button Settings
@@ -38,92 +70,107 @@ const ButtonSettings = () => {
             id="outlined-basic"
             label="button name"
             variant="outlined"
-            sx={{ mr: 2 }}
+            // sx={{ mr: 2 }}
+            value={name}
+            onChange={NamehandleChange}
           />
           <TextField
             id="outlined-basic"
             label="button text"
             variant="outlined"
+            value={text}
+            onChange={TexthandleChange}
           />
         </Box>
+        {/* button position */}
+        <Box className="flex h-48 w-3/5 flex-col items-center justify-center bg-white">
+          <Typography variant="h5" className="mb-8">
+            Button Position
+          </Typography>
+          <div className="flex flex-row">
+            <TextField
+              label="from top to bottom"
+              type="number"
+              variant="outlined"
+              className="mx-2"
+              InputProps={{ inputProps: { min: 0, max: 100 } }}
+              value={top}
+              onChange={TophandleChange}
+            />
+            <TextField
+              label="from left to right"
+              type="number"
+              variant="outlined"
+              InputProps={{ inputProps: { min: 0, max: 100 } }}
+              value={left}
+              onChange={LefthandleChange}
+            />
+          </div>
+        </Box>
+        {/* button style */}
         <Box className="flex flex-col items-start">
           <Box className="my-4">
             <Typography>Button Style</Typography>
             <Stack
               className="mt-8 flex items-start justify-center"
-              direction="row"
               spacing={4}
+              direction={'row'}
             >
               <Paper
-                sx={{
-                  width: 100,
-                  height: 30,
-                  borderRadius: 50,
-                  backgroundColor: '#CA4F79',
-                  cursor: 'pointer',
-                }}
-                onClick={handleClickButtonStyle}
-                // style={{ click == !true ? buttonStyle.Clickable : ''}}
-                {...(click === !click ? buttonStyle.Clickable : '')}
+                className="h-8 w-24 cursor-pointer rounded-full bg-pink"
+                // onClick={handleClickButtonStyle}
+                // // style={{ click == !true ? buttonStyle.Clickable : ''}}
+                // {...(click === !click ? buttonStyle.Clickable : '')}
+                style={buttonStyle.style === 'circle' ? border : {}}
+                onClick={() => handleClickButtonStyle('circle')}
               />
               <Paper
-                sx={{
-                  width: 100,
-                  height: 30,
-                  backgroundColor: '#CA4F79',
-                  cursor: 'pointer',
-                }}
-                onClick={handleClickButtonStyle}
+                className="h-8 w-24 cursor-pointer bg-pink"
+                // onClick={handleClickButtonStyle}
+                style={buttonStyle.style === 'party' ? border : {}}
+                onClick={() => handleClickButtonStyle('party')}
               />
             </Stack>
           </Box>
+          {/* button size */}
           <Box className="my-4">
             <Typography>Button Size</Typography>
             <Stack
-              direction="row"
               spacing={4}
+              direction={'row'}
               className="my-4 flex items-center"
             >
               <Paper
-                sx={{
-                  width: 60,
-                  height: 60,
-                  borderRadius: 50,
-                  backgroundColor: '#CA4F79',
-                  cursor: 'pointer',
-                }}
-                onClick={handleClickButtonStyle}
+                className="h-14 w-14 cursor-pointer rounded-full bg-pink"
+                // onClick={handleClickButtonStyle}
+                onClick={() => handleClickButtonSize('large')}
+                style={buttonStyle.size === 'large' ? border : {}}
               />
               <Paper
-                sx={{
-                  width: 40,
-                  height: 40,
-                  borderRadius: 50,
-                  backgroundColor: '#CA4F79',
-                  cursor: 'pointer',
-                }}
-                onClick={handleClickButtonStyle}
+                className="h-10 w-10 cursor-pointer rounded-full bg-pink"
+                // onClick={handleClickButtonStyle}
+                onClick={() => handleClickButtonSize('medium')}
+                style={buttonStyle.size === 'medium' ? border : {}}
               />
               <Paper
-                sx={{
-                  width: 25,
-                  height: 25,
-                  borderRadius: 50,
-                  backgroundColor: '#CA4F79',
-                  cursor: 'pointer',
-                }}
-                onClick={handleClickButtonStyle}
+                className="h-6 w-6 cursor-pointer rounded-full bg-pink"
+                // onClick={handleClickButtonStyle}
+                onClick={() => handleClickButtonSize('small')}
+                style={buttonStyle.size === 'small' ? border : {}}
               />
             </Stack>
           </Box>
+          {/* button action */}
           <Box>
             <Typography>Button Action</Typography>
-            <Box className="my-4 flex items-center" flexDirection={'column'}>
+            <Box className="my-4 flex flex-col items-center">
               <TextField
                 id="outlined-basic"
                 label="Link to"
                 variant="outlined"
                 sx={{ my: 2 }}
+                value={url}
+                onChange={UrlhandleChange}
               />
               <TextField
                 id="outlined-basic"
@@ -133,19 +180,15 @@ const ButtonSettings = () => {
             </Box>
           </Box>
         </Box>
-        <Stack className="my-4 flex items-start" direction="row" spacing={2}>
+        <Stack className="my-4 flex items-start" spacing={2} direction={'row'}>
           <Button
             size="large"
             variant="contained"
-            sx={{ backgroundColor: '#F3F3F3', color: '#CA4F79' }}
+            className="bg-slate-50 text-pink"
           >
             Cancel
           </Button>
-          <Button
-            size="large"
-            variant="contained"
-            sx={{ width: 110, backgroundColor: '#CA4F79' }}
-          >
+          <Button size="large" variant="contained" className="w-28 bg-pink">
             Save
           </Button>
         </Stack>
