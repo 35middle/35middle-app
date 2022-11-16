881a76b23b9ad104ba6c0950f44b09ebd2112e9f
Anita <scorpioliu1108@gmail.com>
2022-11-16 14:15:38 +0800

    feat: create upload pop up window

--- src/pages/videoUploadPopUpWindow.tsx
+++ src/pages/videoUploadPopUpWindow.tsx
@@ -1,18 +1,46 @@
+// import PopUpWindow from '@/components/popUpWindow';
 import { Box, Button, Card, TextField, Typography } from '@mui/material';
+
+const UploadButton = () => {
+  return (
+    <Button
+      variant="contained"
+      component="label"
+      className="p-5"
+      href="../components/popUpWindow"
+    >
+      Upload Video
+      <input hidden accept="image/*" multiple type="file" />
+    </Button>
+  );
+};
+
+export { UploadButton };
 
 const VideoUploadPopUpWindow = () => {
   return (
     <>
-      <Card>
-        <Box className="bg-white p-10">
+      <Card className="h-112 flex w-80 flex-col justify-between">
+        <Box className="flex h-16 items-center justify-center bg-slate-100">
           <Typography variant="h5">video upload</Typography>
         </Box>
-        <div className="flex flex-col p-10">
-          <div>
+        <div className="flex h-80 flex-col justify-between p-5">
+          <div className="flex h-36 flex-col items-center justify-between">
             <TextField label="Viedeo title" variant="outlined" />
             <TextField label="Viedeo description" variant="outlined" />
           </div>
-          <Button>Upload Video</Button>
+          <Button variant="contained" className="p-5" component="label">
+            Upload Video
+          </Button>
+          {/* <UploadButton onClick={()=> <PopUpWindow /> } /> */}
+          <div className="flex flex-row items-center justify-between">
+            <Button className="p-3" variant="contained">
+              Cancel
+            </Button>
+            <Button className="p-3" variant="contained">
+              Save
+            </Button>
+          </div>
         </div>
       </Card>
     </>
