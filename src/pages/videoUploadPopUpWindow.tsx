884b83081b0f6d2858b649dcbf185579ef65278d
Anita <scorpioliu1108@gmail.com>
2022-11-16 16:40:26 +0800

    feat: create upload messages and upload progress bar

--- src/pages/videoUploadPopUpWindow.tsx
+++ src/pages/videoUploadPopUpWindow.tsx
@@ -1,5 +1,7 @@
-// import { useState } from 'react';
 import { Box, Button, Card, TextField, Typography } from '@mui/material';
+import type { LinearProgressProps } from '@mui/material/LinearProgress';
+import LinearProgress from '@mui/material/LinearProgress';
+import { useEffect, useState } from 'react';
 
 import PopUpWindow from '@/pages/popUpWindow';
 
@@ -16,8 +18,45 @@ const UploadButton = () => {
     </Button>
   );
 };
-
 export { UploadButton };
+
+const LinearProgressWithLabel = (
+  props: LinearProgressProps & { value: number }
+) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+const LinearWithValueLabel = () => {
+  const [progress, setProgress] = useState(10);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 10 : prevProgress + 10
+      );
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  return (
+    <Box sx={{ width: '100%' }}>
+      <LinearProgressWithLabel value={progress} />
+    </Box>
+  );
+};
+export { LinearWithValueLabel };
 
 const VideoUploadPopUpWindow = () => {
   return (
@@ -50,6 +89,20 @@ const VideoUploadPopUpWindow = () => {
           </div>
         </div>
       </Card>
+
+      <Card className="flex h-96 w-80 flex-col justify-between">
+        <Box className="flex h-16 items-center justify-center bg-slate-100">
+          <Typography variant="h5">video upload</Typography>
+        </Box>
+        <div className="flex h-80 w-80 flex-col items-center justify-center bg-gray-300/60 font-bold">
+          <p>Congratulations!</p>
+          <p>Your video has been uploaded.</p>
+          {/* <p>Failed to upload the video.</p>
+          <p>Please try again.</p> */}
+        </div>
+      </Card>
+
+      <LinearWithValueLabel />
     </>
   );
 };
