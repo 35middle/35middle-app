0be981183ba0115c964299aef77950b1dedb5ae9
Leo Liu <leo.liu@domain.com.au>
2022-11-21 23:21:11 +1100

    feat: remove redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,9 @@ import '../styles/global.css';
 
 import { ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
 
 import { theme } from '@/theme';
 import type { UserSession } from '@/types';
-
-import store from '../store/index';
 
 const MyApp = ({
   Component,
@@ -15,9 +12,7 @@ const MyApp = ({
 }: AppProps<{ userSession: UserSession }>) => {
   return (
     <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 };
